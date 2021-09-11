8fde636f9b1e40f13cf2653fbe10d5a19cf3d16e
Thomas Bouder <tbouder@protonmail.com>
2021-09-11 09:38:14 +0200

    🚨 Fixing linting issue

--- components/DialogBox.js
+++ components/DialogBox.js
@@ -6,7 +6,6 @@
 ******************************************************************************/
 
 import	React, {useEffect, useState}	from	'react';
-import	Chevron							from	'components/Chevron';
 import	useKeyPress						from	'hook/useKeyPress';
 
 function	Index({options}) {
