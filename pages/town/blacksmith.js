a43799e5e78727e12b2cba90905b8a226ca996ae
Thomas Bouder <tbouder@protonmail.com>
2021-09-17 17:10:50 +0200

    🚧 wip

--- pages/town/blacksmith.js
+++ pages/town/blacksmith.js
@@ -10,34 +10,55 @@ import	Image							from	'next/image';
 import	useUI							from	'contexts/useUI';
 import	Typer							from	'components/Typer';
 import	DialogBox						from	'components/DialogBox';
+import	SectionArtifactsTheForest			from	'sections/SectionArtifactsTheForest';
 
-function	FacuHeadline() {
+function	DialogChoices({router, adventurersCount}) {
+	if (adventurersCount === 0) {
+		return (
+			<DialogBox
+				options={[
+					{label: 'GO TO THE TAVERN', onClick: () => router.push('/town/tavern?tab=recruit')},
+				]} />
+		);
+	}
+	return (
+		<DialogBox
+			options={[
+				{label: 'WELCOME', onClick: () => router.push('/town/blacksmith')},
+				{label: 'Upgrade an Artifact', onClick: () => router.push('/town/blacksmith?tab=upgrade')},
+				// {label: 'Restore an Artifact', onClick: () => router.push('/town/blacksmith?tab=restore')},
+			]} />
+	);
+}
+
+function	NCPHeadline() {
 	const	[facuTextIndex, set_facuTextIndex] = useState(0);
 	
-	const	renderFacuText = () => {
+	const	renderNCPText = () => {
 		return (
 			<>
 				<Typer onDone={() => set_facuTextIndex(i => i + 1)} shouldStart={facuTextIndex === 0}>
-					{'THE'}
+					{'WELCOME! I AM '}
 				</Typer>&nbsp;
 				<span className={'text-tag-info'}><Typer onDone={() => set_facuTextIndex(i => i + 1)} shouldStart={facuTextIndex === 1}>
-					{'BLACKSMITH'}
+					{'CEAZOR THE BLACKSMITH'}
 				</Typer></span>
 				<Typer onDone={() => set_facuTextIndex(i => i + 1)} shouldStart={facuTextIndex === 2}>
-					{' IS NOT YET OPEN, BUT YOU CAN COME AND HAVE A DRINK IN MY TAVERN IN THE MEAN TIME!'}
+					{'. MY WORKSHOP IS UNDER CONSTRUCTION, BUT IF YOU FOUND SOME ITEMS IN THE FOREST, MAYBE I CAN UPGRADE THEM FOR XP. OR RESTORE THE ONES FROM THE FORMER FOREST.'}
 				</Typer>
 			</>
 		);
 	};
 	return (
 		<h1 className={'text-sm md:text-lg leading-normal md:leading-10'}>
-			{renderFacuText()}
+			{renderNCPText()}
 		</h1>
 	);
 }
 
-function	Index({router}) {
+function	Index({rarities, router}) {
 	const	{theme} = useUI();
+	const	adventurers = Object.values(rarities);
 
 	return (
 		<section className={'mt-12 max-w-full'}>
@@ -51,12 +72,17 @@ function	Index({router}) {
 							width={256}
 							height={256} />
 					</div>
-					<FacuHeadline />
+					<NCPHeadline />
 				</div>
-				<DialogBox
-					options={[
-						{label: 'Go back to the tavern', onClick: () => router.push('/town/tavern')},
-					]} />
+				<DialogChoices
+					router={router}
+					adventurersCount={adventurers.length}
+					adventurer={rarities} />
+				<SectionArtifactsTheForest
+					shouldDisplay={router?.query?.tab === 'upgrade'}
+					router={router}
+					adventurers={rarities}
+					adventurersCount={adventurers.length} />
 			</div>
 		</section>
 	);		
---
+++ sections/SectionArtifactsTheForest.js
@@ -0,0 +1,109 @@
+/******************************************************************************
+**	@Author:				Rarity Extended
+**	@Twitter:				@RXtended
+**	@Date:					Sunday September 12th 2021
+**	@Filename:				SectionArtifactsTheForest.js
+******************************************************************************/
+
+import	React							from	'react';
+import	Image							from	'next/image';
+import	dayjs							from	'dayjs';
+import	relativeTime					from	'dayjs/plugin/relativeTime';
+import	useWeb3							from	'contexts/useWeb3';
+import	useRarity						from	'contexts/useRarity';
+import	{levelUp}						from	'utils/actions';
+import	THE_FOREST_LOOT					from	'utils/theForestLoot';
+import	{xpRequired}					from	'lib/levels';
+
+dayjs.extend(relativeTime);
+
+function	Artifact({img, name, cost, onClick, children, noHover}) {
+	return (
+		<div
+			className={`w-full md:w-60 border-black dark:border-dark-100 border-4 p-4 flex justify-center items-center flex-col ${noHover ? '' : 'group hover:bg-gray-principal dark:hover:bg-dark-100'} transition-colors cursor-pointer relative mb-4 md:mb-0`}
+			onClick={onClick}>
+			<Image
+				src={img}
+				quality={100}
+				width={240}
+				height={240} />
+			<p className={'text-xs justify-center group-hover:underline'}>{name}</p>
+			<p className={'text-xss justify-center text-center mt-1'}>{`Upgrade for ${cost} XP`}</p>
+			{children}
+		</div>
+	);
+}
+
+function	SectionArtifactsTheForest({shouldDisplay, adventurers, router, adventurersCount}) {
+	const	{provider, chainTime} = useWeb3();
+	const	{updateRarity} = useRarity();
+
+	if (!shouldDisplay) {
+		return null;
+	}
+	return (
+		<div className={'flex flex-col w-full'}>
+			<div className={'pb-10'}>
+				<i className={'text-sx md:text-xs text-black dark:text-white text-opacity-60 leading-6'}>
+					{'****'}
+				</i>
+				<div />
+				<i className={'text-sx md:text-xs text-black dark:text-white text-opacity-60 leading-6'}>
+					{'****'}
+				</i>
+				{adventurersCount !== 0 ? <div className={'mt-6'}>
+					<p className={'text-xs'}>
+						{'> WHICH ARTIFACT WOULD YOU LIKE TO UPGRADE?'}
+					</p>
+				</div> :
+					<div className={'mt-6'}> 
+						<p className={'text-xs'}>
+							{'> You first need to recruit an adventurer !'}
+						</p>
+					</div>
+				}
+			</div>
+			<div>
+				<div className={'grid grid-cols-1 md:grid-cols-4 gap-4 md:gap-8'}>
+					{Object.values(adventurers).map((adventurer) => {
+						return (
+							adventurer?.inventory[1].map((item, i) => {
+								return (
+									<Artifact
+										key={`${item.id}_${i}`}
+										onClick={() => {
+											levelUp({provider, contractAddress: process.env.DUNGEON_THE_FOREST_ADDR, tokenID: adventurer.tokenID},
+												({error}) => {
+													if (error) {
+														return console.error(error);
+													}
+													updateRarity(adventurer.tokenID);
+													router.push('/');
+												});
+										}}
+										noHover={Number(adventurer.xp) < xpRequired(item.level)}
+										img={THE_FOREST_LOOT[item.itemName].img}
+										name={item.itemName}
+										cost={xpRequired(item.level)}>
+										{Number(adventurer.xp) < xpRequired(item.level) ?
+											<div className={'absolute inset-0 backdrop-blur-3xl bg-black bg-opacity-60 cursor-not-allowed flex justify-center items-center text-center p-6'}>
+												<p className={'text-white'}>
+													{'YOU NEED MORE XP'}
+												</p>
+											</div>
+											:
+											null
+										}
+									</Artifact>
+								);
+							})
+						);
+					})}
+					
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default SectionArtifactsTheForest;
