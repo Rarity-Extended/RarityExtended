78ab5b757761ef7ae6488a038da84ee971259179
Major-Eth <major-tom.eth@pm.me>
2021-09-21 13:51:03 +0200

    ♻️ Sorting by raruty

--- pages/codex/items.js
+++ pages/codex/items.js
@@ -11,10 +11,33 @@ import	ITEMS						from	'utils/codex/items';
 import	THE_FOREST_LOOT				from	'utils/codex/theForestLoot.json';
 
 function	Index() {
-	const	allItems = [...ITEMS];
-
+	function	sortByRarity(a, b) {
+		let	item1Level = 0;
+		let	item2Level = 0;
+		if (a.level === 'Uncommon')
+			item1Level = 1;
+		if (a.level === 'Rare')
+			item1Level = 2;
+		if (a.level === 'Epic')
+			item1Level = 3;
+		if (a.level === 'Legendary')
+			item1Level = 4;
+		if (a.level === 'Relic')
+			item1Level = 5;
+		if (b.level === 'Uncommon')
+			item2Level = 1;
+		if (b.level === 'Rare')
+			item2Level = 2;
+		if (b.level === 'Epic')
+			item2Level = 3;
+		if (b.level === 'Legendary')
+			item2Level = 4;
+		if (b.level === 'Relic')
+			item2Level = 5;
+		return item1Level - item2Level;
+	}
 	function	renderTheForestLoot() {
-		return Object.values(THE_FOREST_LOOT).map((item) => (
+		return Object.values(THE_FOREST_LOOT).sort(sortByRarity).map((item) => (
 			<div className={'flex flex-row space-x-4 w-full tooltip cursor-help group'} key={`theForest_${item.name}`}>
 				<div className={'w-16 h-16 bg-gray-principal dark:bg-dark-400 flex justify-center items-center item relative'}>
 					<div className={`absolute ${item.levelClassName} left-0 top-0 w-2 h-1`} />
@@ -68,39 +91,6 @@ function	Index() {
 					</div>
 				</div>
 			</div>
-		);
-	}
-	function	renderInventory() {
-		return (
-			allItems.map((item, i) => {
-				if (item.dungeon === 'The Forest') {
-					return renderTheForestLoot();
-				}
-				return (
-					<div className={'flex flex-row space-x-4 w-full tooltip cursor-help group'} key={`${item.id}_${i}`}>
-						<div className={'w-16 h-16 bg-gray-principal dark:bg-dark-400 flex justify-center items-center item relative'}>
-							<div className={`absolute ${item.levelClassName} left-0 top-0 w-2 h-1`} />
-							<div className={`absolute ${item.levelClassName} left-0 top-0 w-1 h-2`} />
-							<div className={`absolute ${item.levelClassName} right-0 top-0 w-2 h-1`} />
-							<div className={`absolute ${item.levelClassName} right-0 top-0 w-1 h-2`} />
-							<Image src={item.img} width={48} height={48} />
-							<div className={`absolute ${item.levelClassName} left-0 bottom-0 w-2 h-1`} />
-							<div className={`absolute ${item.levelClassName} left-0 bottom-0 w-1 h-2`} />
-							<div className={`absolute ${item.levelClassName} right-0 bottom-0 w-2 h-1`} />
-							<div className={`absolute ${item.levelClassName} right-0 bottom-0 w-1 h-2`} />
-						</div>
-						<div className={'text-left flex flex-col py-0.5'}>
-							<p className={'text-xs'}>{item.name}</p>
-						</div>
-						<div className={'tooltiptext invisible group-hover:visible bg-white dark:bg-dark-600 border-4 border-black dark:border-dark-100'}>
-							<div className={'p-4'}>
-								<p className={'text-sx mb-2'}>{item.level}</p>
-								<p className={'text-megaxs mt-4 text-gray-darker dark:text-white dark:text-opacity-60'}>{item.description}</p>
-							</div>
-						</div>
-					</div>
-				);
-			})
 		);
 	}
 
