78061fe480ab762b21d4198aa14036291ca92fe9
Major-Eth <major-tom.eth@pm.me>
2021-09-24 09:44:18 +0200

    ✏️ Adding texts for ceazors

--- pages/town/blacksmith.js
+++ pages/town/blacksmith.js
@@ -33,15 +33,94 @@ function	DialogChoices({router, adventurersCount}) {
 }
 
 function	NCPHeadline({router}) {
-	const	[npcTextIndex, set_npcTextIndex] = useState(0);
 	const	[nonce, set_nonce] = useState(0);
+	const	[npcTextIndex, set_npcTextIndex] = useState(0);
 	
+	const	[hadInitialMessage, set_hadInitialMessage] = useState(false);
+	const	[hadUpgradeMessage, set_hadUpgradeMessage] = useState(false);
+	const	[hadRestoreMessage, set_hadRestoreMessage] = useState(false);
+
 	useEffect(() => {
 		set_npcTextIndex(0);
 		set_nonce(n => n);
 	}, [router?.query?.tab]);
 
 	const	renderNCPText = () => {
+		if (router?.query?.tab === 'upgrade') {
+			if (hadUpgradeMessage) {
+				return (
+					<>
+						{'YOU WENT IN '}
+						<span className={'text-tag-info'}>{'THE FOREST'}</span>
+						{' AND FOUND SOMETHING ? I HAVE SOME STUFF I COULD USE TO '}
+						<span className={'text-tag-info'}>{'UPGRADE'}</span>
+						{' THEM. WHICH ONE DO YOU WANT TO UPGRADE?'}
+					</>		
+				);
+			}
+			return (
+				<>
+					<Typer onDone={() => set_npcTextIndex(i => i + 1)} shouldStart={npcTextIndex === 0}>
+						{'YOU WENT IN '}
+					</Typer>&nbsp;
+					<span className={'text-tag-info'}><Typer onDone={() => set_npcTextIndex(i => i + 1)} shouldStart={npcTextIndex === 1}>
+						{'THE FOREST'}
+					</Typer></span>
+					<Typer onDone={() => set_npcTextIndex(i => i + 1)} shouldStart={npcTextIndex === 2}>
+						{' AND FOUND SOMETHING ? I HAVE SOME STUFF I COULD USE TO '}
+					</Typer>
+					<span className={'text-tag-info'}><Typer onDone={() => set_npcTextIndex(i => i + 1)} shouldStart={npcTextIndex === 3}>
+						{'UPGRADE'}
+					</Typer></span>
+					<Typer
+						onDone={() => {
+							set_npcTextIndex(i => i + 1);
+							set_hadUpgradeMessage(true);
+						}}
+						shouldStart={npcTextIndex === 4}>
+						{' THEM. WHICH ONE DO YOU WANT TO UPGRADE?'}
+					</Typer>
+				</>
+			);
+		}
+		if (router?.query?.tab === 'restore') {
+			if (hadRestoreMessage) {
+				return (
+					<>
+						{'IF YOU HAVE SOME RUSTY ARTIFACTS FROM '}
+						<span className={'text-tag-info'}>{'THE OLD FOREST'}</span>
+						{', I CAN RESTORE THEM FOR FREE!'}
+					</>		
+				);
+			}
+			return (
+				<>
+					<Typer onDone={() => set_npcTextIndex(i => i + 1)} shouldStart={npcTextIndex === 0}>
+						{'IF YOU HAVE SOME RUSTY ARTIFACTS FROM '}
+					</Typer>&nbsp;
+					<span className={'text-tag-info'}><Typer onDone={() => set_npcTextIndex(i => i + 1)} shouldStart={npcTextIndex === 1}>
+						{'THE OLD FOREST'}
+					</Typer></span>
+					<Typer
+						onDone={() => {
+							set_npcTextIndex(i => i + 1);
+							set_hadRestoreMessage(true);
+						}}
+						shouldStart={npcTextIndex === 2}>
+						{', I CAN RESTORE THEM FOR FREE!'}
+					</Typer>
+				</>
+			);
+		}
+		if (hadInitialMessage) {
+			return (
+				<>
+					{'WELCOME! I AM '}
+					<span className={'text-tag-info'}>{'CEAZOR THE BLACKSMITH'}</span>
+					{'. MY WORKSHOP IS UNDER CONSTRUCTION, BUT IF YOU FOUND SOME ITEMS IN THE FOREST, MAYBE I CAN UPGRADE THEM FOR XP. OR RESTORE THE ONES FROM THE FORMER FOREST.'}
+				</>		
+			);
+		}
 		return (
 			<>
 				<Typer onDone={() => set_npcTextIndex(i => i + 1)} shouldStart={npcTextIndex === 0}>
@@ -50,7 +129,12 @@ function	NCPHeadline({router}) {
 				<span className={'text-tag-info'}><Typer onDone={() => set_npcTextIndex(i => i + 1)} shouldStart={npcTextIndex === 1}>
 					{'CEAZOR THE BLACKSMITH'}
 				</Typer></span>
-				<Typer onDone={() => set_npcTextIndex(i => i + 1)} shouldStart={npcTextIndex === 2}>
+				<Typer
+					onDone={() => {
+						set_npcTextIndex(i => i + 1);
+						set_hadInitialMessage(true);
+					}}
+					shouldStart={npcTextIndex === 2}>
 					{'. MY WORKSHOP IS UNDER CONSTRUCTION, BUT IF YOU FOUND SOME ITEMS IN THE FOREST, MAYBE I CAN UPGRADE THEM FOR XP. OR RESTORE THE ONES FROM THE FORMER FOREST.'}
 				</Typer>
 			</>
