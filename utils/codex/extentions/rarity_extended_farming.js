b3ac81cee116537ca97649490162bcec0d973f8e
Major-Eth <major-tom.eth@pm.me>
2022-02-21 14:27:42 +0100

    ➕ Adding Murderteeth farms

--- utils/codex/extentions/rarity_extended_farming.js
+++ utils/codex/extentions/rarity_extended_farming.js
@@ -19,7 +19,7 @@ const WOOD_TIERS = [
 		width: 152,
 		height: 248,
 		cost: [
-			{...LOOTS[process.env.RARITY_EXTENDED_WOOD_LOOT_0], amount: 12}]
+			{...LOOTS[process.env.LOOT_WOOD_ADDR], amount: 12}]
 	},
 	{
 		name: 'Fine Wood',
@@ -29,8 +29,8 @@ const WOOD_TIERS = [
 		width: 152,
 		height: 248,
 		cost: [
-			{...LOOTS[process.env.RARITY_EXTENDED_WOOD_LOOT_0], amount: 6},
-			{...LOOTS[process.env.RARITY_EXTENDED_WOOD_LOOT_1], amount: 36}
+			{...LOOTS[process.env.LOOT_WOOD_ADDR], amount: 6},
+			{...LOOTS[process.env.LOOT_WOOD_SOFT_ADDR], amount: 36}
 		]
 	},
 	{
@@ -41,9 +41,9 @@ const WOOD_TIERS = [
 		width: 152,
 		height: 248,
 		cost: [
-			{...LOOTS[process.env.RARITY_EXTENDED_WOOD_LOOT_0], amount: 6},
-			{...LOOTS[process.env.RARITY_EXTENDED_WOOD_LOOT_1], amount: 18},
-			{...LOOTS[process.env.RARITY_EXTENDED_WOOD_LOOT_2], amount: 72},
+			{...LOOTS[process.env.LOOT_WOOD_ADDR], amount: 6},
+			{...LOOTS[process.env.LOOT_WOOD_SOFT_ADDR], amount: 18},
+			{...LOOTS[process.env.LOOT_WOOD_FINE_ADDR], amount: 72},
 		]
 	},
 	{
@@ -54,10 +54,10 @@ const WOOD_TIERS = [
 		width: 294,
 		height: 271,
 		cost: [
-			{...LOOTS[process.env.RARITY_EXTENDED_WOOD_LOOT_0], amount: 6},
-			{...LOOTS[process.env.RARITY_EXTENDED_WOOD_LOOT_1], amount: 18},
-			{...LOOTS[process.env.RARITY_EXTENDED_WOOD_LOOT_2], amount: 36},
-			{...LOOTS[process.env.RARITY_EXTENDED_WOOD_LOOT_3], amount: 120},
+			{...LOOTS[process.env.LOOT_WOOD_ADDR], amount: 6},
+			{...LOOTS[process.env.LOOT_WOOD_SOFT_ADDR], amount: 18},
+			{...LOOTS[process.env.LOOT_WOOD_FINE_ADDR], amount: 36},
+			{...LOOTS[process.env.LOOT_WOOD_SEASONED_ADDR], amount: 120},
 		]
 	},
 	{
@@ -68,11 +68,11 @@ const WOOD_TIERS = [
 		width: 241,
 		height: 212,
 		cost: [
-			{...LOOTS[process.env.RARITY_EXTENDED_WOOD_LOOT_0], amount: 6},
-			{...LOOTS[process.env.RARITY_EXTENDED_WOOD_LOOT_1], amount: 18},
-			{...LOOTS[process.env.RARITY_EXTENDED_WOOD_LOOT_2], amount: 36},
-			{...LOOTS[process.env.RARITY_EXTENDED_WOOD_LOOT_3], amount: 60},
-			{...LOOTS[process.env.RARITY_EXTENDED_WOOD_LOOT_4], amount: 180},
+			{...LOOTS[process.env.LOOT_WOOD_ADDR], amount: 6},
+			{...LOOTS[process.env.LOOT_WOOD_SOFT_ADDR], amount: 18},
+			{...LOOTS[process.env.LOOT_WOOD_FINE_ADDR], amount: 36},
+			{...LOOTS[process.env.LOOT_WOOD_SEASONED_ADDR], amount: 60},
+			{...LOOTS[process.env.LOOT_WOOD_HARD_ADDR], amount: 180},
 		]
 	},
 ];
@@ -82,7 +82,7 @@ const ORE_TIERS = [
 		name: 'Copper Ore',
 		address: process.env.RARITY_EXTENDED_ORE_FARM_0,
 		tier: 0,
-		img: `/items/${process.env.RARITY_EXTENDED_ORE_LOOT_0}.png`,
+		img: `/items/${process.env.LOOT_ORE_COPPER_ADDR}.png`,
 		width: 256,
 		height: 256,
 		cost: [],
@@ -92,82 +92,95 @@ const ORE_TIERS = [
 		name: 'Iron Ore',
 		address: process.env.RARITY_EXTENDED_ORE_FARM_1,
 		tier: 1,
-		img: `/items/${process.env.RARITY_EXTENDED_ORE_LOOT_1}.png`,
+		img: `/items/${process.env.LOOT_ORE_IRON_ADDR}.png`,
 		width: 256,
 		height: 256,
 		cost: [
-			{...LOOTS[process.env.RARITY_EXTENDED_ORE_LOOT_0], amount: 12}
+			{...LOOTS[process.env.LOOT_ORE_COPPER_ADDR], amount: 12}
 		]
 	},
 	{
 		name: 'Gold Ore',
 		address: process.env.RARITY_EXTENDED_ORE_FARM_2,
 		tier: 2,
-		img: `/items/${process.env.RARITY_EXTENDED_ORE_LOOT_2}.png`,
+		img: `/items/${process.env.LOOT_ORE_GOLD_ADDR}.png`,
 		width: 256,
 		height: 256,
 		cost: [
-			{...LOOTS[process.env.RARITY_EXTENDED_ORE_LOOT_0], amount: 6},
-			{...LOOTS[process.env.RARITY_EXTENDED_ORE_LOOT_1], amount: 36}
+			{...LOOTS[process.env.LOOT_ORE_COPPER_ADDR], amount: 6},
+			{...LOOTS[process.env.LOOT_ORE_IRON_ADDR], amount: 36}
 		]
 	},
 	{
 		name: 'Platinum Ore',
 		address: process.env.RARITY_EXTENDED_ORE_FARM_3,
 		tier: 3,
-		img: `/items/${process.env.RARITY_EXTENDED_ORE_LOOT_3}.png`,
+		img: `/items/${process.env.LOOT_ORE_PLATINIUM_ADDR}.png`,
 		width: 256,
 		height: 256,
 		cost: [
-			{...LOOTS[process.env.RARITY_EXTENDED_ORE_LOOT_0], amount: 6},
-			{...LOOTS[process.env.RARITY_EXTENDED_ORE_LOOT_1], amount: 18},
-			{...LOOTS[process.env.RARITY_EXTENDED_ORE_LOOT_2], amount: 72},
+			{...LOOTS[process.env.LOOT_ORE_COPPER_ADDR], amount: 6},
+			{...LOOTS[process.env.LOOT_ORE_IRON_ADDR], amount: 18},
+			{...LOOTS[process.env.LOOT_ORE_GOLD_ADDR], amount: 72},
+		]
+	},
+	{
+		name: 'Cold Iron Ore',
+		address: process.env.RARITY_MURDERTEETH_COLD_IRON_FARM,
+		tier: 3,
+		img: `/items/${process.env.LOOT_ORE_COLD_IRON}.png`,
+		width: 256,
+		height: 256,
+		cost: [
+			{...LOOTS[process.env.LOOT_ORE_COPPER_ADDR], amount: 6},
+			{...LOOTS[process.env.LOOT_ORE_IRON_ADDR], amount: 18},
+			{...LOOTS[process.env.LOOT_ORE_GOLD_ADDR], amount: 72},
 		]
 	},
 	{
 		name: 'Mithril Ore',
 		address: process.env.RARITY_EXTENDED_ORE_FARM_4,
 		tier: 4,
-		img: `/items/${process.env.RARITY_EXTENDED_ORE_LOOT_4}.png`,
+		img: `/items/${process.env.LOOT_ORE_MITHRIL_ADDR}.png`,
 		width: 256,
 		height: 256,
 		cost: [
-			{...LOOTS[process.env.RARITY_EXTENDED_ORE_LOOT_0], amount: 6},
-			{...LOOTS[process.env.RARITY_EXTENDED_ORE_LOOT_1], amount: 18},
-			{...LOOTS[process.env.RARITY_EXTENDED_ORE_LOOT_2], amount: 36},
-			{...LOOTS[process.env.RARITY_EXTENDED_ORE_LOOT_3], amount: 120},
+			{...LOOTS[process.env.LOOT_ORE_COPPER_ADDR], amount: 6},
+			{...LOOTS[process.env.LOOT_ORE_IRON_ADDR], amount: 18},
+			{...LOOTS[process.env.LOOT_ORE_GOLD_ADDR], amount: 36},
+			{...LOOTS[process.env.LOOT_ORE_PLATINIUM_ADDR], amount: 120},
 		]
 	},
 	{
 		name: 'Orichalcum Ore',
 		address: process.env.RARITY_EXTENDED_ORE_FARM_5,
 		tier: 5,
-		img: `/items/${process.env.RARITY_EXTENDED_ORE_LOOT_5}.png`,
+		img: `/items/${process.env.LOOT_ORE_ORICHALCUM_ADDR}.png`,
 		width: 256,
 		height: 256,
 		cost: [
-			{...LOOTS[process.env.RARITY_EXTENDED_ORE_LOOT_0], amount: 6},
-			{...LOOTS[process.env.RARITY_EXTENDED_ORE_LOOT_1], amount: 18},
-			{...LOOTS[process.env.RARITY_EXTENDED_ORE_LOOT_2], amount: 36},
-			{...LOOTS[process.env.RARITY_EXTENDED_ORE_LOOT_3], amount: 60},
-			{...LOOTS[process.env.RARITY_EXTENDED_ORE_LOOT_4], amount: 180},
+			{...LOOTS[process.env.LOOT_ORE_COPPER_ADDR], amount: 6},
+			{...LOOTS[process.env.LOOT_ORE_IRON_ADDR], amount: 18},
+			{...LOOTS[process.env.LOOT_ORE_GOLD_ADDR], amount: 36},
+			{...LOOTS[process.env.LOOT_ORE_PLATINIUM_ADDR], amount: 60},
+			{...LOOTS[process.env.LOOT_ORE_MITHRIL_ADDR], amount: 180},
 		]
 	},
-	// {
-	// 	name: 'Adamantine Ore',
-	// 	address: process.env.RARITY_EXTENDED_ORE_FARM_5,
-	// 	tier: 5,
-	// 	img: '/items/farming/ore-adamantine.png',
-	// 	width: 256,
-	// 	height: 256,
-	// 	cost: [
-	// 		{...LOOTS[process.env.RARITY_EXTENDED_ORE_LOOT_0], amount: 6},
-	// 		{...LOOTS[process.env.RARITY_EXTENDED_ORE_LOOT_1], amount: 18},
-	// 		{...LOOTS[process.env.RARITY_EXTENDED_ORE_LOOT_2], amount: 36},
-	// 		{...LOOTS[process.env.RARITY_EXTENDED_ORE_LOOT_3], amount: 60},
-	// 		{...LOOTS[process.env.RARITY_EXTENDED_ORE_LOOT_4], amount: 180},
-	// 	]
-	// },
+	{
+		name: 'Adamantine Ore',
+		address: process.env.RARITY_MURDERTEETH_ADAMANTINE_FARM,
+		tier: 5,
+		img: `/items/${process.env.LOOT_ORE_ADAMANTINE}.png`,
+		width: 256,
+		height: 256,
+		cost: [
+			{...LOOTS[process.env.LOOT_ORE_COPPER_ADDR], amount: 6},
+			{...LOOTS[process.env.LOOT_ORE_IRON_ADDR], amount: 18},
+			{...LOOTS[process.env.LOOT_ORE_GOLD_ADDR], amount: 36},
+			{...LOOTS[process.env.LOOT_ORE_COLD_IRON], amount: 180},
+			{...LOOTS[process.env.LOOT_ORE_MITHRIL_ADDR], amount: 90},
+		]
+	},
 ];
 
 export {WOOD_TIERS, ORE_TIERS};
